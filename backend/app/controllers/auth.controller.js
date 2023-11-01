da8c1347e2b8493982ff32a12b8fe7b8a41f69f6
julytf <julytf.liyc@gmail.com>
2023-11-01 15:25:59 +0700

    wip: update auth.controller

--- backend/app/controllers/auth.controller.js
+++ backend/app/controllers/auth.controller.js
@@ -1,28 +1,27 @@
 const config = require("./app/config");
+const User = require("../models/User.model");
 
-// exports.login = async function (req, res, next) {
-//   const user = await User.findOne({
-//     username: req.body.username,
-//     password: req.body.password,
-//   });
+exports.register = async function (req, res, next) {
+  const user = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
-//   if (!user) return next(new Error("Unauthorized!"));
+  const token = jwt.sign(
+    {
+      _id: user._id,
+    },
+    config.jwt.key
+  );
 
-//   const token = jwt.sign(
-//     {
-//       _id: user._id,
-//     },
-//     config.jwt.key
-//   );
-
-//   return res.status(200).json({
-//     status: "success",
-//     token,
-//     data: {
-//       user,
-//     },
-//   });
-// };
+  return res.status(200).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
 
 exports.login = async function (req, res, next) {
   const user = await User.findOne({
